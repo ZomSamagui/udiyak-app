bdb252464d758edc43908412f904821e0261b0e3
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-11-13 14:35:31 +0900

    Create :: Nav Type 설정

---
+++ src/type/navigation.type.ts
@@ -0,0 +1,9 @@
+export type RootStackParamList = {
+    OnBoarding: undefined;
+    Auth: undefined;
+    Main: undefined;
+    Map: {
+        initialLat?: number;
+        initialLng?: number;
+    };
+};
